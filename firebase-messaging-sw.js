f0448b21b695f42d903276cc464274e9b2fb69d5
Ferlaty <davidmato330@gmail.com>
2025-12-28 16:56:51 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -20,7 +20,8 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon.png'
+    icon: '/favicon.ico'
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
+
 });
